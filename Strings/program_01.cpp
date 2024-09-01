488d83d6e5f2e9a993cf7b0de758edd355f2daac
Vijay Maurya <115324275+Vijay6265@users.noreply.github.com>
2024-09-01 13:24:49 +0000

    string

---
+++ Strings/program_01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    // string str = "Hello dosto ";
+  
+    // cout<<endl;
+    // cout<<str;
+
+    // string str1 = "fam";
+    // string str2 = "ily";
+    // cout<< str1 + str2<<endl;
+
+
+    // string str1 = "fam";
+    // string str2 = "ily";
+    // cout<<str1[1]<<endl;
+
+
+    
+
+
+    return 0;
+
+}
